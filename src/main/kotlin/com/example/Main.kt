6446053b9e7c1def4465c4f11250c5c0b37bee4d
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2019-01-25 18:32:16 +0100

    Include newUser html and post request handler

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -4,13 +4,26 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
+import io.ktor.request.receiveParameters
 import io.ktor.response.*
 import io.ktor.routing.*
+
+val users = mutableListOf<String>()
 
 fun Application.main() {
     routing {
         get("/") {
             call.respondText("Hello world", ContentType.Text.Html)
+        }
+
+        post("/newUser") {
+            val post = call.receiveParameters()
+            val userName = post["userName"]
+            if (userName != null) {
+                users.add(userName)
+            }
+
+            call.respondText("List of created users: $users", ContentType.Text.Html)
         }
 
         static("static") {
