8492caa8972d858eaa557cdfec35367218c002c0
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2019-01-25 18:16:37 +0100

    Move server configuration to an external resource

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -4,17 +4,11 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 
-fun Application.module() {
+fun Application.main() {
     routing {
         get("/") {
             call.respondText("Hello world", ContentType.Text.Html)
         }
     }
-}
-
-fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080, module = Application::module).start(wait = true)
 }
