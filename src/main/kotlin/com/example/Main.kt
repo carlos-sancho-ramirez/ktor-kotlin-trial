448c42aeef9dcd7760de74d5354dfdadce509e5c
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2019-01-29 18:45:37 +0100

    Replace keys within the html response

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -7,8 +7,48 @@ import io.ktor.http.content.static
 import io.ktor.request.receiveParameters
 import io.ktor.response.*
 import io.ktor.routing.*
+import java.io.InputStream
+import java.io.OutputStream
+
+const val newUserPath = "/newUserX"
 
 val users = mutableListOf<String>()
+
+fun replaceTexts(inStream: InputStream, outStream: OutputStream, map: Map<String, String>) {
+    do {
+        var byte = inStream.read()
+        if (byte >= 0) {
+            if (byte == '$'.toInt()) {
+                byte = inStream.read()
+                if (byte >= 0) {
+                    if (byte == '{'.toInt()) {
+                        byte = inStream.read()
+                        var key = ""
+                        while (byte >= 0 && byte != '}'.toInt()) {
+                            key += byte.toChar()
+                            byte = inStream.read()
+                        }
+
+                        val text = if (byte == '}'.toInt()) map.get(key) ?: "\${$key}" else "\${$key"
+                        for (ch: Char in text) {
+                            outStream.write(ch.toInt())
+                        }
+                    }
+                    else {
+                        outStream.write('$'.toInt())
+                        outStream.write(byte)
+                    }
+                }
+                else {
+                    outStream.write('$'.toInt())
+                }
+            }
+            else {
+                outStream.write(byte)
+            }
+        }
+    } while (byte >= 0)
+}
 
 fun Application.main() {
     routing {
@@ -16,7 +56,7 @@ fun Application.main() {
             call.respondText("Hello world", ContentType.Text.Html)
         }
 
-        post("/newUser") {
+        post(newUserPath) {
             val post = call.receiveParameters()
             val userName = post["userName"]
             if (userName != null) {
@@ -26,8 +66,12 @@ fun Application.main() {
             call.respondText("List of created users: $users", ContentType.Text.Html)
         }
 
-        static("static") {
-            resources("html")
+        get("/newUserForm") {
+            call.respondOutputStream(ContentType.Text.Html, HttpStatusCode.OK) {
+                val inStream = this::class.java.getResourceAsStream("/html/newUser.html")
+                val map = mapOf("newUserPath" to newUserPath)
+                replaceTexts(inStream, this, map)
+            }
         }
 
         static("css") {
@@ -35,3 +79,5 @@ fun Application.main() {
         }
     }
 }
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
